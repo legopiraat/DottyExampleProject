bd3e17453faeb8de9256b9ac9697d5c3e4e6fedc
Legopiraat <gijs.venne@hotmail.com>
2018-07-04 16:22:49 +0200

    Remove akward implicit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,8 +2,6 @@ import Validator._
 
 object Main {
 
-  implicit val discountRate: Double = 0.1
-
   def main(args: Array[String]): Unit = {
       printValidation(validateInt(5, 5))
       printValidation(validateLong(5L, 5L))
